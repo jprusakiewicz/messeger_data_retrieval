cd5d88e558e36d743cc6737a78e775d4c2a15162
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2020-01-09 00:15:17 +0100

    feat messages printing

--- chat_data_retrieval.py
+++ chat_data_retrieval.py
@@ -1,19 +1,47 @@
 import requests
 import json
 
+#request for only one person 'https://graph.facebook.com/v5.0/t_2705948632785028/messages?access_token=EAAjc4szTP2ABANM2ZAX7BR65Uo8EHSkmQF54GEcSVEhZAx6tXpZCRvVckHnDv7eb8ul2ObnLzGrpUu5CL4zURVCX7wxSYlt20GZAf9Ca7rROH5Ir2wecEV2iXVFbxS4KmdqwtZBKcKlYpJP9lgUb1JLZC2Yo8e39iAcl467VeTIcHlAyNGZAJBK&fields=message&limit=25&after=QVFIUmpSQWN3VGZAJZAm9kaFZATbGFhY3FNNUU4X0U4NTZAGSldvRFBrUVplZAm9qZAHBkTVRnTzRtZA1lQM2xPR3ZAaeWxZAbFpYNjRJaERRRkphRDU2b0ZAJOGxTZAU11WDJOWkd3T2JOMmhsNjJ3SWczNDFDUmF4ZAFQ5SGlCR29rOFgydWZArU2xq'
 single_conv = 'https://graph.facebook.com/v3.3/108525050657214/'
-
-acc_token = '&access_token=EAAjc4szTP2ABANzI5tV6QLZBaicCLTbrf7adKtQYW5YQwq1hG88rgwPbeo6udhMRlyhxV7Q5pmLuyYl6ZBgX8PM9lFqkAZCfozDNyp66fj3ZBZCxmdEaNtViaz1PNXWlM5zBXooAZCvJqYpueuW24RGbrYk3xZCUCfDmdNd7AZCAdOuBezUTBEiqFjZC7dlGc9ZBVxS5kcEZBgCIQZDZD'
-
-conversation = 'https://graph.facebook.com/v3.3/108525050657214/conversations?fields=participants,link&limit=400{0}'.format(acc_token)
+acc_token = '&access_token=EAAjc4szTP2ABANM2ZAX7BR65Uo8EHSkmQF54GEcSVEhZAx6tXpZCRvVckHnDv7eb8ul2ObnLzGrpUu5CL4zURVCX7wxSYlt20GZAf9Ca7rROH5Ir2wecEV2iXVFbxS4KmdqwtZBKcKlYpJP9lgUb1JLZC2Yo8e39iAcl467VeTIcHlAyNGZAJBK'
+conversation = 'https://graph.facebook.com/v3.3/108525050657214/conversations?fields=participants%2Clink%2cmessages{{message}}{0}'.format(acc_token)
 r = requests.get(conversation)
 
-
-
 print(r.json()['data'][0])
+print(r.json()['data'][1]) #not all data ->care for next
 conv_data = r.json()['data']
-for i in range(0, len(conv_data)):
+mes = []
+person = []
+for i in range(0, len(conv_data)):  #dla każdego rozmówcy
     conv_id = conv_data[i]['id']
     print(conv_data[i]['id'])
-    r_conv = requests.get('https://graph.facebook.com/v3.3/108525050657214/{0}'.format(acc_token))
-    print(r_conv)
+    print(conv_data[i]['messages']['data'])
+    print(conv_data[i]['participants']['data'])
+    #print(conv_data[i]['id'])
+    #r_conv = requests.get('https://graph.facebook.com/v3.3/108525050657214/{0}'.format(acc_token))
+    person.append(conv_data[i]['participants']['data'][0]['name'])
+    mes.append((conv_data[i]['messages']['data']))
+
+
+
+messages = conv_data[1]['messages']['data'] #wiadomości tylko z klara
+for i in range(0, len(messages)):
+    # print(messages[i])
+    print(messages[i]['message'])
+
+print(type(messages))
+for i in range(0, len(mes)):
+    print("person: {}".format(person[i]))
+    print(mes[i][0]['message']) #print only first question from everyone
+
+
+print("nowy")
+j = 0 #person index
+for i in range(0, len(mes[j])):
+    print(mes[j][i]['message']) #print all from person index
+
+for i in range (0, len(mes)):
+    print("person: {}".format(person[i]))
+    for i in range(0, len(mes[i])):
+        print(mes[j][i]['message']) #print all for all
+
