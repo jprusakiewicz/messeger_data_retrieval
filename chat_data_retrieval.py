9cdd91771913517640e83eaa3e4a7681e2954518
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2020-01-09 17:29:58 +0100

    feat pprint

--- chat_data_retrieval.py
+++ chat_data_retrieval.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import pprint
 
 #request for only one person 'https://graph.facebook.com/v5.0/t_2705948632785028/messages?access_token=EAAjc4szTP2ABANM2ZAX7BR65Uo8EHSkmQF54GEcSVEhZAx6tXpZCRvVckHnDv7eb8ul2ObnLzGrpUu5CL4zURVCX7wxSYlt20GZAf9Ca7rROH5Ir2wecEV2iXVFbxS4KmdqwtZBKcKlYpJP9lgUb1JLZC2Yo8e39iAcl467VeTIcHlAyNGZAJBK&fields=message&limit=25&after=QVFIUmpSQWN3VGZAJZAm9kaFZATbGFhY3FNNUU4X0U4NTZAGSldvRFBrUVplZAm9qZAHBkTVRnTzRtZA1lQM2xPR3ZAaeWxZAbFpYNjRJaERRRkphRDU2b0ZAJOGxTZAU11WDJOWkd3T2JOMmhsNjJ3SWczNDFDUmF4ZAFQ5SGlCR29rOFgydWZArU2xq'
 #single_conv = 'https://graph.facebook.com/v3.3/108525050657214/'
 acc_token = open("token.txt", "r").read()
 conversation = 'https://graph.facebook.com/v3.3/114322838608649/conversations?fields=participants%2Clink%2cmessages{{message}}{0}'.format(acc_token)
 r = requests.get(conversation)
-
+#todo
 conv_data = r.json()['data']
 data_json = {}
 message = []
@@ -25,7 +26,8 @@ for mes in new_mes.values():
         mes[i] = mes[i]['message'] #ecstract message only, cant do it in line 17
 
 with open('data.txt', 'w', encoding='utf-8') as outfile:
-     json.dump(new_mes, outfile, ensure_ascii=False)
+    json.dump(new_mes, outfile, ensure_ascii=False)
 
-
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(new_mes)
 
